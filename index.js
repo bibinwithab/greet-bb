487ceb229acf6c6e3b6cddda8fae4c178ab9a26e
bibinwithab <bibingraceson@gmail.com>
2023-08-21 16:37:51 +0530

    working with chalk

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
 const figlet = require('figlet');
+const chalk = require('chalk');
 
 const prompt = require('prompt-sync')();
 
-async function greet() {
-    console.log(("Who is this?"));
+function greet() {
+    console.log(chalk.cyan("Who is this?"));
     let name = prompt(">");
 
     figlet(`Hello ${name}`, (err, data) => {
         if (err) {
-            console.log(`Hello ${name}`);
+            console.log(`Hello name-less person`);
         } else {
             console.log((data));
         }
