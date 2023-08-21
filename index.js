bcf866ef1ae8867836eb68b5820fb09e5aa43932
bibinwithab <bibingraceson@gmail.com>
2023-08-21 17:23:02 +0530

    used cli args and included -h and -v

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 #!/usr/bin/env node
 
 const figlet = require('figlet');
-const fs = require('fs');
 
-const prompt = require('prompt-sync')();
 
-function greet() {
-    console.log("Who is this?");
-    let name = prompt(">");
+const packageVersion = "1.0.3";
 
-    figlet(`Hello ${name}`, (err, data) => {
-        if (err) {
-            console.log(`Hello name-less person`);
-        } else {
+function help(){
+    console.log('Usage: greet [OPTIONS] <or> [MESSAGE]');
+    console.log('  -h, --help\t\t\tDisplay this help message');
+    console.log('  -v, --version\t\t\tDisplay the version of greet');
+}
+
+function version(){
+    console.log('Version: ' + packageVersion);
+}
+
+function call(){
+    const args = process.argv.slice(2);
+        if (args.length == 0) {
+            greet(args[0]);
+        } 
+        else if (args.length == 1) {
+            if (args[0] == '-h' || args[0] == '--help') {
+                help();
+            } else if (args[0] == '-v' || args[0] == '--version') {
+                version();
+            } else {
+                greet(args[0]);
+            }
+        }
+}
+
+function greet(n){
+    const name = n;
+    callBoss(name);
+    figlet( `Hello ${name}`, (err,data) => {
+        if(err){
+            console.log('Hello nameless being');
+        }
+        else{
             console.log((data));
         }
     });
 }
 
-greet();
+
+call();
+
